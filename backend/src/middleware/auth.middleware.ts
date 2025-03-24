c2031adea365a63eb8a9e97e2c80ddab120d754c
RIRI141 <167848567+RIRI141@users.noreply.github.com>
2025-03-23 17:10:13 -0700

    Revert "Organizedesign"

--- backend/src/middleware/auth.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session || !req.session.isLoggedIn) {
-        return res.status(401).json({ error: "Unauthorized" });
-    } else  res.status(200).json({ isLoggedIn: true, userId: req.session.userId });
-    next();
-};
--- frontend/src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css"
-
-function Footer() {
-    return (
-        <footer><p className="copyRight">Copyright © RTOGA ISHII All Rights Reserved.</p></footer>
-    )
-        
-}
-export default Footer
